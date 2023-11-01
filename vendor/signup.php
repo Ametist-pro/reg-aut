236e84219d838cbc4e555a74626abe9105401f6b
Ametist-pro <59925480+Ametist-pro@users.noreply.github.com>
2023-11-02 00:33:32 +0300

    Add files via upload

---
+++ vendor/signup.php
@@ -0,0 +1,27 @@
+<?php
+    global $connect;
+    session_start();
+    require_once 'connect.php';
+
+    $name = $_POST['name'];
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $password_confirm = $_POST['password_confirm'];
+
+    if ($password === $password_confirm) {
+
+
+        $password = md5($password);
+
+        mysqli_query($connect, "INSERT INTO `user` (`id`,`name`,`login`,`password`) VALUES (NULL, '$name', '$login', '$password')");
+
+        $_SESSION['message'] = 'Регистрация прошла успешно!';
+        header('Location: ../index.php');
+
+
+    } else {
+        $_SESSION['message'] = 'Пароли не совпадают';
+        header('Location: ../register.php');
+    }
+
+?>
